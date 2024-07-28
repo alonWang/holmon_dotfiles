7bc345de15551c814d8ac16680265bdf323ceda8
al <wangweilong1996@gmail.com>
2024-07-28 12:59:29 +0800

    python

--- python/install.sh
+++ python/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 curl https://pyenv.run | bash
-pyenv init -
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 pyenv install 3.12.4
 pyenv global 3.12.4
 pip install --user pipx
